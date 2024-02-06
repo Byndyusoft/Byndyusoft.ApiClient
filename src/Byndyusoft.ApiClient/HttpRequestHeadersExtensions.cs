fd7dcac52567b384e2300b1f9ff6155e35541b41
zaparivannyi <zaparivannyi@byndyusoft.com>
2024-02-06 17:43:22 +0300

    Добавлен проброс хедеров

---
+++ src/Byndyusoft.ApiClient/HttpRequestHeadersExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace Byndyusoft.ApiClient
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net.Http.Headers;
+
+    public static class HttpRequestHeadersExtensions
+    {
+        public static HttpRequestHeaders AddRange(this HttpRequestHeaders headers, Dictionary<string, string> newHeaders)
+        {
+            if (newHeaders.Any() == false)
+                return headers;
+
+            foreach (var newHeaderKey in newHeaders.Keys)
+            {
+                headers.Add(newHeaderKey, newHeaders[newHeaderKey]);
+            }
+
+            return headers;
+        }
+    }
+}
